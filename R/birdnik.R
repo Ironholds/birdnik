905321ca27d97f1742049c82bab84b7102de2f69
ironholds <ironholds@gmail.com>
2016-08-13 09:23:15 -0400

    docs

---
+++ R/birdnik.R
@@ -0,0 +1,12 @@
+#' @title Connector to the Wordnik API
+#' @name birdnik
+#' @description This package provides functions for the Wordnik API.
+#'
+#' @seealso \code{\link{word_definitions}}, \code{\link{word_top_example}},
+#' \code{\link{related_words}}, \code{\link{word_pronunciations}},
+#' \code{\link{word_frequency}}, \code{\link{word_bigrams}}, and
+#' \code{\link{birdnik_get}}.
+#'
+#' @docType package
+#' @aliases birdnik birdnik-package
+NULL
